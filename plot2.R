3bb06da028e3ae7bd2fa3f4ca259218f4a3f032e
guillaume <imbert.guillaume@gmail.com>
2014-06-08 17:06:54 +0200

    plot 1-4 release (plot 1 modified)

--- plot2.R
+++ plot2.R
@@ -1,11 +1,11 @@
-plot1 <- function() {
+plot2 <- function() {
   zip<-unz("exdata-data-household_power_consumption.zip",filename="household_power_consumption.txt")
   data <- read.csv(zip,  sep=";", na.strings=c("", " ", "?"), as.is=c("Date", "Time"))
   data$DateTime <- paste(data$Date, data$Time)
   data$DateTime <- strptime(data$DateTime,format="%d/%m/%Y %H:%M:%S")
   data$Date <- strptime(data$Date, format="%d/%m/%Y")
   a <- data[(data$Date >= strptime("2007-02-01", format="%Y-%m-%d") ) & ( data$Date <= strptime("2007-02-02", format="%Y-%m-%d")),]
-  png(filename="plot1.png", width=480, height=480, units="px")
-  hist(x=a$Global_active_power, breaks=20, xlab="Global Active Power(kilowatts)", main="Global Active Power", col="red")
+  png(filename="plot2.png", width=480, height=480, units="px")
+  plot(x=a$DateTime,y=a$Global_active_power, type="l", ylab="Global Active Power(kilowatts)", xlab="")
   dev.off()
 }
